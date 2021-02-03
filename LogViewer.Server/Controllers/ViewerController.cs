e15b15a9d15d77178c2514526357965a1d58c66c
Zdravko Patekar Bahun <z.patekarbahun@printecgroup.com>
2021-02-03 14:18:03 +0100

    Allow reloading the log while open elsewhere

--- LogViewer.Server/Controllers/ViewerController.cs
+++ LogViewer.Server/Controllers/ViewerController.cs
@@ -1,126 +1,127 @@
-﻿using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using LogViewer.Server.Extensions;
+﻿using LogViewer.Server.Extensions;
 using LogViewer.Server.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.IO;
 
 namespace LogViewer.Server.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class ViewerController : ControllerBase
-    {
-        private readonly ILogger<ViewerController> _logger;
-        private readonly ILogParser _logParser;
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ViewerController : ControllerBase
+	{
+		private readonly ILogger<ViewerController> _logger;
+		private readonly ILogParser _logParser;
 
-        public ViewerController(ILogger<ViewerController> logger, ILogParser logParser)
-        {
-            _logger = logger;
-            _logParser = logParser;
-        }
+		public ViewerController(ILogger<ViewerController> logger, ILogParser logParser)
+		{
+			_logger = logger;
+			_logParser = logParser;
+		}
 
 
-        [HttpGet("open")]
-        public ActionResult<string> Open(string filePath)
-        {     
+		[HttpGet("open")]
+		public ActionResult<string> Open(string filePath)
+		{
 
-            //Check for valid filepath
-            if (System.IO.File.Exists(filePath) == false)
-            {
-                var message = $"No file exists on disk at {filePath}";
-                return NotFound(message);
-            }
+			//Check for valid filepath
+			if (System.IO.File.Exists(filePath) == false)
+			{
+				var message = $"No file exists on disk at {filePath}";
+				return NotFound(message);
+			}
 
-            //Check for file extension ends with one of the following
-            //.json .txt or .clef
+			//Check for file extension ends with one of the following
+			//.json .txt or .clef
 
-            //Don't want to attempt to any old file type
-            var extension = Path.GetExtension(filePath);
-            if (extension != ".txt" && extension != ".json" && extension != ".clef")
-            {
-                var message = $"The file {filePath} is not a compatible log file. Can only open .json, .txt or .clef files";
-                return BadRequest(message);
-            }
+			//Don't want to attempt to any old file type
+			var extension = Path.GetExtension(filePath);
+			if (extension != ".txt" && extension != ".json" && extension != ".clef")
+			{
+				var message = $"The file {filePath} is not a compatible log file. Can only open .json, .txt or .clef files";
+				return BadRequest(message);
+			}
 
-            //Lets check file is valid JSON & not a text document on your upcoming novel
-            var firstLine = System.IO.File.ReadLines(filePath).First();
+			string firstLine;
+			using (var s = System.IO.File.Open(filePath, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+			using (var sr = new StreamReader(s))
+			{ firstLine = sr.ReadLine(); }
 
-            if (firstLine.IsValidJson() == false)
-            {
-                var message = $"The file {filePath} does not contain valid JSON on line one";
-                return BadRequest(message);
-            }
+			if (firstLine.IsValidJson() == false)
+			{
+				var message = $"The file {filePath} does not contain valid JSON on line one";
+				return BadRequest(message);
+			}
 
-            //We will skip over/ignore invalid/malformed log lines
-            var logs = _logParser.ReadLogs(filePath);            
-            return $"Log contains {logs.Count}";
+			//We will skip over/ignore invalid/malformed log lines
+			var logs = _logParser.ReadLogs(filePath);
+			return $"Log contains {logs.Count}";
 
-        }
+		}
 
-        [HttpGet("reload")]
-        public ActionResult<string> Reload()
-        {
-            //Ensure _logFilePath not null
-            if(string.IsNullOrEmpty(_logParser.LogFilePath) == false)
-            {
-                //Call Open again with the stored path
-                return Open(_logParser.LogFilePath);
-            }
+		[HttpGet("reload")]
+		public ActionResult<string> Reload()
+		{
+			//Ensure _logFilePath not null
+			if (string.IsNullOrEmpty(_logParser.LogFilePath) == false)
+			{
+				//Call Open again with the stored path
+				return Open(_logParser.LogFilePath);
+			}
 
-            return Ok();
-        }
+			return Ok();
+		}
 
-        [HttpGet("totals")]
-        public ActionResult<LogLevelCounts> TotalCounts()
-        {
-            if(_logParser.LogIsOpen == false)
-                return BadRequest("No logfile has been opened yet");
-            
-            return _logParser.TotalCounts();
-        }
+		[HttpGet("totals")]
+		public ActionResult<LogLevelCounts> TotalCounts()
+		{
+			if (_logParser.LogIsOpen == false)
+				return BadRequest("No logfile has been opened yet");
 
-        [HttpGet("errors")]
-        public ActionResult<int> TotalErrors()
-        {
-            if (_logParser.LogIsOpen == false)
-                return BadRequest("No logfile has been opened yet");
+			return _logParser.TotalCounts();
+		}
 
-            return _logParser.TotalErrors();
-        }
+		[HttpGet("errors")]
+		public ActionResult<int> TotalErrors()
+		{
+			if (_logParser.LogIsOpen == false)
+				return BadRequest("No logfile has been opened yet");
 
-        [HttpGet("export")]
-        public ActionResult Export(string messageTemplate, string newFileName)
-        {
-            if (_logParser.LogIsOpen == false)
-                return BadRequest("No logfile has been opened yet");
+			return _logParser.TotalErrors();
+		}
 
-            if (string.IsNullOrEmpty(newFileName))
-                return BadRequest("Missing filename to export the JSON log file");
+		[HttpGet("export")]
+		public ActionResult Export(string messageTemplate, string newFileName)
+		{
+			if (_logParser.LogIsOpen == false)
+				return BadRequest("No logfile has been opened yet");
 
-            _logParser.ExportTextFile(messageTemplate, newFileName);
-            return Ok(); //TODO: Maybe return something useful in response - stream file or ...?! GUI can show loader?!
-        }
-        
-        [HttpGet("messagetemplates")]
-        public ActionResult<List<LogTemplate>> MessageTemplates()
-        {
-            if (_logParser.LogIsOpen == false)
-                return BadRequest("No logfile has been opened yet");
+			if (string.IsNullOrEmpty(newFileName))
+				return BadRequest("Missing filename to export the JSON log file");
 
-            return _logParser.GetMessageTemplates();
-            
-        }
+			_logParser.ExportTextFile(messageTemplate, newFileName);
+			return Ok(); //TODO: Maybe return something useful in response - stream file or ...?! GUI can show loader?!
+		}
 
-        [HttpGet("search")]
-        public ActionResult<PagedResult<LogMessage>> Search(int pageNumber = 1, int pageSize = 100, string filterExpression = null, SortOrder sort = SortOrder.Descending)
-        {
-            if (_logParser.LogIsOpen == false)
-                return BadRequest("No logfile has been opened yet");
+		[HttpGet("messagetemplates")]
+		public ActionResult<List<LogTemplate>> MessageTemplates()
+		{
+			if (_logParser.LogIsOpen == false)
+				return BadRequest("No logfile has been opened yet");
 
-            return _logParser.Search(pageNumber, pageSize, filterExpression, sort);
-        }       
+			return _logParser.GetMessageTemplates();
 
-    }
+		}
+
+		[HttpGet("search")]
+		public ActionResult<PagedResult<LogMessage>> Search(int pageNumber = 1, int pageSize = 100, string filterExpression = null, SortOrder sort = SortOrder.Descending)
+		{
+			if (_logParser.LogIsOpen == false)
+				return BadRequest("No logfile has been opened yet");
+
+			return _logParser.Search(pageNumber, pageSize, filterExpression, sort);
+		}
+
+	}
 }
